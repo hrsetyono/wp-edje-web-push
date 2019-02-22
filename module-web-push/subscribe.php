4c035e6afcd1e8552e9df0c8d6017282355ab78e
Henner Setyono <henner.renardi@gmail.com>
2019-02-22 14:27:01 +0700

    Update subscribe.php

--- module-web-push/subscribe.php
+++ module-web-push/subscribe.php
@@ -23,27 +23,37 @@ class WebPush_Subscribe {
   function subscribe_api( $request ) {
     $data = $request->get_params();
 
-    // check if already subscribed
-    $already_subscribe = get_comments( array(
-      'author_email' => $data['h_push_id'], 'number' => 1
-    ) );
+    if( !isset( $data['content'] ) ) {
+      return 'ERROR - Subscription not saved due to missing POST data';
+    }
 
-    if( $already_subscribe ) {
-      return 'This user is already subscribed';
+    // Check if already subscribed
+    $push_id = substr( $data['content'], -15, -3 );
+    if( $this->_already_subscribed( $push_id ) ) {
+      return 'ERROR - This user is already subscribed';
     }
 
     $comment_args = array(
       'comment_post_ID' => 0,
       'comment_author' => 'Edje',
-      'comment_author_email' => $data['h_push_id'],
+      'comment_author_email' => $push_id,
       'comment_author_url' => 'wp-edje-web-push',
       'comment_content' => $data['content'],
-      'comment_type' => $data['topic'],
-      'user_id' => $data['user_id'],
+      'comment_type' => isset( $data['topic'] ) ? $data['topic'] : '',
+      'user_id' => isset( $data['user_id'] ) ? $data['user_id'] : 0,
       'comment_parent' => 0,
     );
 
     return wp_insert_comment( $comment_args );
+  }
+
+
+  ////
+
+  private function _already_subscribed( $push_id ) {
+    return get_comments( array(
+      'author_email' => $push_id, 'number' => 1
+    ) );
   }
 
 }
